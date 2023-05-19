641596fc037c43d4cb639dfe37b66fc9764bc3e4
권민정 <22alswjd@naver.com>
2023-05-20 01:19:22 +0900

    fix: merge 오류 수정

--- src/main/java/edu/ssafy/enjoytrip/config/WebMvcConfiguration.java
+++ src/main/java/edu/ssafy/enjoytrip/config/WebMvcConfiguration.java
@@ -1,33 +1,12 @@
 package edu.ssafy.enjoytrip.config;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import edu.ssafy.enjoytrip.interceptor.LoginInterceptor;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-
-//	private final List<String> patterns = Arrays.asList(
-//				"/information/write", "/information/modify/*", "/information/delete/*", 
-//				"/api/information/write", "/api/information/modify/*", "/api/information/delete/*");
-//	
-//	private LoginInterceptor loginInterceptor;
-//	
-//	public WebMvcConfiguration(LoginInterceptor loginInterceptor) {
-//		this.loginInterceptor = loginInterceptor;
-//	}
-	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(loginInterceptor)
-//				.addPathPatterns(patterns);
-//	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
